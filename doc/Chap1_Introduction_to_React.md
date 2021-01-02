a1feb24ff592431cccd8288332603cef4dc88eab
Sunwook Eom <sunwook5492@gmail.com>
2021-01-02 17:52:31 +0900

    Chap1.9: Managing multiple input states
    Signed-off-by: Sunwook Eom <sunwook5492@gmail.com>

--- doc/Chap1_Introduction_to_React.md
+++ doc/Chap1_Introduction_to_React.md
@@ -18,4 +18,17 @@ return <div>안녕하세요</div>;
 ### Babel
 Babel 은 자바스크립트의 문법을 확장해주는 도구이며 리액트 컴포넌트 파일에서 XML 형태로 코드를 작성하면 babel 이 JSX 를 JavaScript 로 변환을 해줌  
 
+## Notice
+### 불변성 지키기
+리액트에서 객체를 업데이트하게 될 때에는 기존 객체를 직접 수정하면 안되고, 새로운 객체를 만들어서, 새 객체에 변화를 주어야 한다.  
+```javascript
+// X
+inputs[name] = value;
 
+// O
+setInputs({
+      ...inputs,
+        [name]: value
+        });
+
+```
--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-    const [text, setText] = useState('');
+    const [eom_inputs, setEomInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+
+    const {name,nickname} = eom_inputs;
 
     const onChange = (e) => {
-        setText(e.target.value);
+        const {value, name} = e.target;
+        setEomInputs({
+            ...eom_inputs,
+            [name]: value
+        })
     };
 
     const onReset = () => {
-        setText('');
+        setEomInputs({
+            name: '',
+            nickname: '',
+        })
     };
 
     return (
         <div>
-            <input onChange={onChange} value={text} />
+            <input name="name" placeholder="Name" onChange={onChange} value={name} />
+            <input name="nickname" placeholder="NickName" onChange={onChange} value={nickname} />
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: {text} </b>
+                <b>값: </b>
+                {name} ({nickname})
             </div>
         </div>
     );
